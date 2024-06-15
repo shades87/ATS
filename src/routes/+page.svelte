2ca426ba3d7e1f752951598ac686bfb4ff6a9c82
Daniel McFadyen <shadyshadyshades@hotmail.com>
2024-06-15 20:53:26 +1000

    selection values and on change handlers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,20 @@
 		import { FileDropzone } from '@skeletonlabs/skeleton';
 		import { TabGroup, Tab } from '@skeletonlabs/skeleton';
 
+		let user = {
+			age: 0,
+			ed: 0,
+			nat: 0,
+			city: 0,
+			income: 0
+		}
+		let age = 0;
 		let tabSet: number = 0;
+		
+		
+		const handleAge = () => {
+			age = user.age;
+		}
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
@@ -13,37 +26,37 @@
 			<h1 class="h1" style="text-align: center;">Please add a .pdf or a news article URL</h1>
 			<br>
 			<div class="flex align-items">
-				<p>Age </p>
-				<select class="Demographic" style="margin-left: 5px;" color="secondary-500">
+				<p style="margin-left: 10px; margin-right: 10px;">Age </p>
+				<select class="select"  color="secondary-500"  bind:value={user.age} on:change={handleAge}>
 					<option value="0"></option>
 					<option value="1">Under 15</option>
 					<option value="2">15-35</option>
 					<option value="3">36-65</option>
 					<option value="4">66+</option>
 				</select>
-				<p  style="margin-left: 10px;"> Education Level </p>
-				<select class="Demographic"  style="margin-left: 5px;">
+				<p  style="margin-left: 10px; margin-right: 10px;"> Education Level </p>
+				<select class="select">
 					<option value="0"></option>
 					<option value="1">High School</option>
 					<option value="2">Bachelor's degree</option>
 					<option value="3">PHD</option>
 				</select>
-				<p  style="margin-left: 10px;"> Nationality </p>
-				<select class="Demographic"  style="margin-left: 5px;">
+				<p  style="margin-left: 10px; margin-right: 10px;"> Nationality </p>
+				<select class="select">
 					<option value="0"></option>
 					<option value="1">Australian</option>
 					<option value="2">New Zealand</option>
 					<option value="3">England</option>
 					<option value="3">United States</option>
 				</select>
-				<p  style="margin-left: 10px;"> Metro or regional </p>
-				<select class="Demographic"  style="margin-left: 5px;">
+				<p  style="margin-left: 10px; margin-right: 10px;"> Metro or regional </p>
+				<select class="select">
 					<option value="0"></option>
 					<option value="1">Metro</option>
 					<option value="2">Regional</option>
 				</select>
-				<p style="margin-left: 10px;">Income level</p>
-				<select class="Demographic"  style="margin-left: 5px;">
+				<p style="margin-left: 10px; margin-right: 10px;">Income level</p>
+				<select class="select">
 					<option value="0"></option>
 					<option value="1">Under $30K</option>
 					<option value="2">$30K-$100K</option>
