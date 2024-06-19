93c7dd9bafae421648e28e3017c49011460e3a9d
Daniel McFadyen <shadyshadyshades@hotmail.com>
2024-06-19 21:00:55 +1000

    "connecting selections"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 <script lang=ts>
 		import { FileDropzone } from '@skeletonlabs/skeleton';
 		import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+	import type { Stringifier } from 'postcss';
 
 		let user = {
 			age: 0,
@@ -11,12 +12,113 @@
 			income: 0
 		}
 
-		let age = 0;
+		let age:string = "";
+		let ed:string = "";
+		let nat:string = "";
+		let city:string = "";
+		let income:string = "";
+
 		let tabSet: number = 0;
 		
 		
 		const handleAge = () => {
-			age = user.age;
+			console.log(user.age)
+			switch (+user.age) {
+				case 1:
+					console.log("in case 1:")
+					age = "under 15";
+					console.log(age)
+					break;
+				case 2:
+					age = "15 to 35";
+					break;
+				case 3:
+					age = "35-65";
+					break;
+				case 4:
+					age = "65+";
+					break;
+				case 0:
+					age: "";
+					break;
+			}
+			
+		}
+
+		const handleEd = () => {
+			switch (+user.ed) {
+				case 1:
+					ed = "high school";
+					break;
+				case 2:
+					ed = "university";
+					break;
+				case 3:
+					ed = "PHD";
+					break;
+				case 0:
+					ed = "";
+					break;
+			}
+		}
+
+		const handleNat = () => {
+			switch (+user.nat) {
+				case 1:
+					nat = "Australian";
+					break;
+				case 2:
+					nat = "New Zealand";
+					break;
+				case 3:
+					nat = "England";
+					break;
+				case 4:
+					nat = "US";
+					break;
+				case 0:
+					nat = "";
+					break;
+			}
+		}
+
+		const handleCity = () => {
+			switch (+user.city) {
+				case 1:
+					city = "metro";
+					break;
+				case 2:
+					city = "regional";
+					break;
+				case 0:
+					city = "";
+					break;
+			}
+		}
+
+		const handleIncome = () => {
+			switch (+user.income) {
+				case 1:
+					income = "under 30K";
+					break;
+				case 2:
+					income = "30K-100K";
+					break;
+				case 3:
+					income = "100K+";
+					break;
+				case 0:
+					income = "";
+					break;
+			}
+		}
+
+		const clearAll = () =>{
+			user.age=0; age = "";
+			user.ed = 0; ed = "";
+			user.nat = 0; nat = "";
+			user.city = 0; city = "";
+			user.income = 0; income = "";
 		}
 </script>
 
@@ -37,14 +139,14 @@
 					<option value="4">66+</option>
 				</select>
 				<p  style="margin-left: 10px; margin-right: 10px;"> Education Level </p>
-				<select class="select">
+				<select class="select" bind:value={user.ed} on:change={handleEd}>
 					<option value="0"></option>
 					<option value="1">High School</option>
 					<option value="2">Bachelor's degree</option>
 					<option value="3">PHD</option>
 				</select>
 				<p  style="margin-left: 10px; margin-right: 10px;"> Nationality </p>
-				<select class="select">
+				<select class="select" bind:value={user.nat} on:change={handleNat}>
 					<option value="0"></option>
 					<option value="1">Australian</option>
 					<option value="2">New Zealand</option>
@@ -52,13 +154,13 @@
 					<option value="3">United States</option>
 				</select>
 				<p  style="margin-left: 10px; margin-right: 10px;"> Metro or regional </p>
-				<select class="select">
+				<select class="select" bind:value={user.city} on:change={handleCity}>
 					<option value="0"></option>
 					<option value="1">Metro</option>
 					<option value="2">Regional</option>
 				</select>
 				<p style="margin-left: 10px; margin-right: 10px;">Income level</p>
-				<select class="select">
+				<select class="select" bind:value={user.income} on:change={handleIncome}>
 					<option value="0"></option>
 					<option value="1">Under $30K</option>
 					<option value="2">$30K-$100K</option>
@@ -66,7 +168,7 @@
 				</select>
 				
 			</div>
-			<button type="button" class="btn variant-filled float-right" style="margin: 10px;">clear all</button>
+			<button type="button" class="btn variant-filled float-right" style="margin: 10px;" on:click={clearAll}>clear all</button>
 			<br>
 			<br>
 		</div>
@@ -98,6 +200,12 @@
 		<bv>
 		<br>
 		<br>
+		<p>
+			User is {age}, {city}, {ed}, {income} and {nat}
+		</p>
+		<p>
+			Selection codes {user.age}, {user.city}, {user.ed}, {user.income} and {user.nat}
+		</p>
 		<ul>
 			<li><code class="code"><a href="https://github.com/shades87">https://github.com/shades87</a></code> - github</li>
 		</ul>
